15f96e0104e677a2d5e6a0c58d0fb17159c2cfd4
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-06 13:58:52 +0100

    Ejercicio 2

--- UD6/src/ejercicios/Ejercicio2.java
+++ UD6/src/ejercicios/Ejercicio2.java
@@ -1,10 +1,33 @@
 package ejercicios;
 
+import java.util.Random;
+
+//import java.util.Random;
+//import java.util.stream.IntStream;
+
+import javax.swing.JOptionPane;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random aleatorio = new Random();
+		Random aleatorio2 = new Random(); 
+		String arreu1 = JOptionPane.showInputDialog("Dime un primer numero aleatorio: ");
+		int numrandom = Integer.parseInt(arreu1);
+		aleatorio.setSeed(numrandom);
+		String arreu2 = JOptionPane.showInputDialog("Dime un segundo numero aleatorio");
+		int numrandom2 = Integer.parseInt(arreu2);
+		aleatorio2.setSeed(numrandom2);
+		int num = (int)(Math. random()*(numrandom2-numrandom))+numrandom;
+		JOptionPane.showMessageDialog(null,"Primer número: \n" +numrandom+"\n"+"Segundo número: \n"+numrandom2+"\n Numero aleatorio: \n"+num);
+		
+		//int calculo = Math.random() * (numrandom+1);
+		/*int calculo = (int) (Math.random()*numrandom);*/
+		//JOptionPane.showMessageDialog(null, aleatorio);
+		/*JOptionPane.showMessageDialog(null, calculo);*/
+		//Random random = new Random();
+		//IntStream = random.ints(numrandom);
 	}
 
 }
