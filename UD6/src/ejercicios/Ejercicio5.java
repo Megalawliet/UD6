535d1c0abc02adf341b035cbbf9b5ff01136eb1b
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-08 20:22:07 +0100

    Ejercicio 5

--- UD6/src/ejercicios/Ejercicio5.java
+++ UD6/src/ejercicios/Ejercicio5.java
@@ -1,10 +1,36 @@
 package ejercicios;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// metemos por pantalla un numero decimal
+		// String Pantalla = JOptionPane.showInputDialog("Pon un numero decimal: ");
+		// int convierte = Integer.parseInt(Pantalla);
+		int r = 0;
+		conversion(r);
 
 	}
 
+	public static int conversion(int n) {
+		String Pantalla = JOptionPane.showInputDialog("Pon un numero decimal: ");
+		int convierte = Integer.parseInt(Pantalla);
+		String binario = "";
+		int num;
+
+		do {
+
+			num = convierte % 2;
+			binario += num;
+			convierte = convierte / 2;
+
+		} while (convierte != 0);
+		// reversion de la cadena de binario a traves de la funcionalidad java de
+		// reverse
+		binario = new StringBuilder(binario).reverse().toString();
+		System.out.println("Numero Binario de " + Pantalla + " es: " + binario);
+		return convierte;
+	}
 }
