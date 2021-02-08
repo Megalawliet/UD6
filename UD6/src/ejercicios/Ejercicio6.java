dc7c31313ff100fcc08da1b7a30294a183dedba5
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-08 21:08:25 +0100

    Ejercicio 6

--- UD6/src/ejercicios/Ejercicio6.java
+++ UD6/src/ejercicios/Ejercicio6.java
@@ -1,5 +1,31 @@
 package ejercicios;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 0;
+		cifras(a);
+
+	}
+
+	public static int cifras(int n) {
+		// demanes per pantalla valors en forma de cadena
+		String dar = JOptionPane.showInputDialog("Pon una serie de numeros");
+		// crees una variable que et faigue el conteig per la funcio length a través del
+		// numero
+		// de caracters que li passes per pantalla
+		int contar = dar.length();
+		// fas que segons els caracters donats, anira recorrent les posicions i sumant 1
+		// fins que s'acabe la cadena
+		for (int i = 0; i <= contar; i++) {
+
+		}
+		System.out.println("EL numero es: " + contar);
+		return contar;
+	}
 
 }
