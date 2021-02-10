6c3232770a8e21814eec3fd51bd104ae490d1ae2
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-10 15:36:21 +0100

    Ejercicio 8

--- UD6/src/ejercicios/Ejercicio8.java
+++ UD6/src/ejercicios/Ejercicio8.java
@@ -1,10 +1,22 @@
 package ejercicios;
-
+import javax.swing.JOptionPane;
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//array de 10 posicions
+		int numeros[] = new int[10];
+		rellenar(numeros);
+		mostrar(numeros);
 	}
 
+	public static void rellenar(int lista[]) {
+			for(int i=0;i<lista.length;i++) {
+				lista[i] = Integer.parseInt(JOptionPane.showInputDialog("Introducir valores de relleno: "));//lista
+		}
+	}
+	public static void mostrar (int posiciones[]){
+		for(int i=0;i<posiciones.length;i++) {
+			System.out.println("En la posición : ["+i+"] el array guarda el numero: "+posiciones[i]);
+		}
+	}
 }
